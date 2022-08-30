615f1acf1d26885ca058f5060bc7cc41cda2dab1
Bruno.R <bcr04@yahoo.com.br>
2022-08-30 16:14:34 -0300

    exercicio01

---
+++ ecommerce-store/src/main/java/br/letscode/models/Cliente.java
@@ -0,0 +1,38 @@
+// package br.letscode.models;
+
+// import javax.persistence.Column;
+// import javax.persistence.Entity;
+// import javax.persistence.GeneratedValue;
+// import javax.persistence.Id;
+// import javax.persistence.Table;
+// import javax.persistence.GenerationType;
+
+
+// @Entity
+// @Table(name= "Cliente")
+// public class Cliente {
+    
+// @Id
+// @GeneratedValue(strategy = GenerationType.IDENTITY)
+// private long id;
+
+// @Column(name = "nome")
+// private String nome;
+
+// @Column(name = "sobrenome")
+// private String sobrenome;
+
+// @Column(name = "email")
+// private String email;
+
+// @Column(name = "sexo")
+// private String sexo;
+
+// @Column(name = "cpf")
+// private String cpf;
+
+
+
+
+
+// }
---
+++ ecommerce-store/src/main/java/br/letscode/services/TimeServiceImpl.java
@@ -0,0 +1,26 @@
+package br.letscode.services;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+import java.time.temporal.ChronoUnit;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Service;
+
+@Service
+public class TimeServiceImpl implements TimeService {
+
+    Logger log = LoggerFactory.getLogger(TimeServiceImpl.class);  
+
+    public String retornarHoraAtual(){
+        LocalTime dataHora = LocalTime.now();        
+        String retorno = dataHora.truncatedTo(ChronoUnit.SECONDS).toString();
+        log.info("Hora atual: "+ retorno);
+        return retorno; 
+    }
+
+    
+
+}
